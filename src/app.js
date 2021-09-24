239d3bd336d05370fc5592e01135a50c43c1ee81
bbanderson <tombyun@naver.com>
2021-09-25 08:51:05 +0900

    Add polyfill with core-js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import MainController from "./controllers/MainController.js";
 import "./main.css";
 
+(() => {
+  new Promise();
+})();
+
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
 });
