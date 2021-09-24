d19d053ef66fb36a7795084b7ed87e449e81db44
bbanderson <tombyun@naver.com>
2021-09-24 18:37:50 +0900

    Add file-loader

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [path.resolve("./my-webpack-loader.js")],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+        },
+      },
+    ],
+  },
+};
