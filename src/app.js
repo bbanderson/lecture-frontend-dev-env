9df2c0b7a9eb57e6cad0d5f6eb1f7de0cb98a15a
bbanderson <tombyun@naver.com>
2021-09-24 18:56:08 +0900

    Add url-loader

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import MainController from "./controllers/MainController.js";
 import "./app.css";
-// import nyancat from "./nyancat.jpg";
+import nyancat from "./nyancat.jpeg";
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
-  // document.body.innerHTML = `
-  //   <img src="${nyancat}"/>
-  // `;
+  document.body.innerHTML = `
+    <img src="${nyancat}"/>
+  `;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,12 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: "url-loader",
         options: {
           publicPath: "./dist/",
           name: "[name].[ext]?[hash]",
+          limit: 20000, // 20KB 이하 파일에 한해 base64(main.js 내 문자열로 변환) 적용, 20KB 초과시 file-loader(파일 그대로 복사) 적용
         },
       },
     ],
