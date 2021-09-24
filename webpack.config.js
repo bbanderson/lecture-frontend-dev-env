84092d7d1c45932d1d11fd66d20cb844638ba99a
bbanderson <tombyun@naver.com>
2021-09-24 21:59:47 +0900

    Add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/", // index.html 입장에서는 이미지가 ./dist에 위치하기 때문
+          name: "[name].[ext]?[hash]",
+          limit: 10000,
+        },
+      },
+    ],
+  },
+};
